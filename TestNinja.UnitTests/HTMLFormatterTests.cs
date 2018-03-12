8964cd3662088bfbb669a9a2620b924fbfc4fb8f
smartguy05 <smartguy05@gmail.com>
2018-03-12 13:33:49 -0600

    Testing arrays and strings

---
+++ TestNinja.UnitTests/HTMLFormatterTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HTMLFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+
+            var result = formatter.FormatAsBold("abc");
+
+            // Specific 
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase);
+
+            //More General
+            //Assert.That(result, Does.StartWith("<strong>"));
+            //Assert.That(result, Does.EndWith("</strong>"));
+            //Assert.That(result, Does.Contain("abc"));
+        }
+    }
+}
