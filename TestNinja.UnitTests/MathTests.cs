d4d4b3421abfe7b33cd56a67a2d673b8b8f10725
smartguy05 <smartguy05@gmail.com>
2018-03-12 11:50:55 -0600

    NUnit with setup

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Math = TestNinja.Fundamentals.Math;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        private Math _math;
+
+        // Setup
+        // Teardown
+        [SetUp]
+        public void Setup()
+        {
+            _math = new Math();
+        }
+
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var result = _math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_FirstArgIsGreater_ReturnFirstArg()
+        {
+            var result = _math.Max(2, 1);
+            
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_SecondArgIsGreater_ReturnSecondArg()
+        {
+            var result = _math.Max(1, 2);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+        
+        [Test]
+        public void Max_ArgsAreEqual_ReturnTheSameArgument()
+        {
+            var result = _math.Max(1, 1);
+
+            Assert.That(result, Is.EqualTo(1));
+        }
+    }
+}
