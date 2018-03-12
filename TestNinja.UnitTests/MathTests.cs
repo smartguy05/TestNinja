8a0a82549355b15553f1886ab3ff08ec818b37b7
smartguy05 <smartguy05@gmail.com>
2018-03-12 12:44:16 -0600

    Ignore attribute

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -22,6 +22,7 @@ namespace TestNinja.UnitTests
         }
 
         [Test]
+        [Ignore("Because I wanted to!")]
         public void Add_WhenCalled_ReturnTheSumOfArguments()
         {
             var result = _math.Add(1, 2);
