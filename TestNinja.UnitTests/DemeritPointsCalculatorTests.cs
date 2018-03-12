624f98ab19a0fe92246367f94044adee5a3b0875
smartguy05 <smartguy05@gmail.com>
2018-03-12 15:09:01 -0600

    Exercise 1

---
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        private DemeritPointsCalculator _dmCalculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _dmCalculator = new DemeritPointsCalculator();
+            
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_SpeedLessThanZeroOrGreaterThanMax_ThrowOutOfRangeException(int speed)
+        {
+            Assert.That(() => _dmCalculator.CalculateDemeritPoints(speed), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        [TestCase(0,0)]
+        [TestCase(30,0)]
+        [TestCase(65,0)]
+        [TestCase(66,0)]
+        [TestCase(70,1)]
+        [TestCase(75,2)]
+        public void CalculateDemeritPoints_WhenCalled_ReturnDemeritPoints(int speed, int demerit)
+        {
+            var result = _dmCalculator.CalculateDemeritPoints(speed);
+
+            Assert.That(result, Is.EqualTo(demerit));
+        }
+    }
+}
---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutput_NumberDivisibleBy3And5_ReturnsFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+        
+
+        [Test]
+        public void GetOutput_NumberDivisibleBy3ButNot5_ReturnsFizz()
+        {
+            var result = FizzBuzz.GetOutput(3);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void GetOutput_NumberDivisibleBy5ButNot3_ReturnsBuzz()
+        {
+            var result = FizzBuzz.GetOutput(5);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void GetOutput_NumberNotDivisibleBy3Or5_ReturnsNumber()
+        {
+            var result = FizzBuzz.GetOutput(2);
+
+            Assert.That(result, Is.EqualTo("2"));
+        }
+    }
+}
