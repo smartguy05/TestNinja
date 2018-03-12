1560cd1dd4716a81406602cd618acbc0f9a113d5
smartguy05 <smartguy05@gmail.com>
2018-03-12 13:58:44 -0600

    Errors and events

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0);
+
+            // Not Found
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+
+            //Not Found or one of it's derivatives
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+
+        }
+    }
+}
