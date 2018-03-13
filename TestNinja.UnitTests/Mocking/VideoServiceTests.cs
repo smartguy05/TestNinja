e2d1b72c1ac0890aed5d74f9a6115c6596db02d7
smartguy05 <smartguy05@gmail.com>
2018-03-13 08:52:10 -0600

    dependency injection

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -15,9 +15,10 @@ namespace TestNinja.UnitTests.Mocking
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
-            var service = new VideoService();
+            var service = new VideoService(new FakeFileReader());
 
-            var result = service.ReadVideoTitle(new FakeFileReader());
+            //var result = service.ReadVideoTitle(new FakeFileReader());
+            var result = service.ReadVideoTitle();
 
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
--- TestNinja/Mocking/VideoService.cs
+++ TestNinja/Mocking/VideoService.cs
@@ -9,9 +9,18 @@ namespace TestNinja.Mocking
 {
     public class VideoService
     {
-        public string ReadVideoTitle(IFileReader fileReader)
+        private IFileReader _fileReader;
+
+        public VideoService(IFileReader fileReader = null)
         {
-            var str = fileReader.Read("video.txt");
+            _fileReader = fileReader ?? new FileReader();
+        }
+        
+        //public string ReadVideoTitle(IFileReader fileReader)
+        public string ReadVideoTitle()
+        {
+            //var str = fileReader.Read("video.txt");
+            var str = _fileReader.Read("video.txt");
             var video = JsonConvert.DeserializeObject<Video>(str);
             if (video == null)
                 return "Error parsing the video.";
