f65b31346d634bf3675ef82c7b04d688ca2e9a39
smartguy05 <smartguy05@gmail.com>
2018-03-13 10:50:07 -0600

    controller/db/entity mocking exercise

---
+++ TestNinja.UnitTests/Mocking/EmployeeControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class EmployeeControllerTests
+    {
+        [Test]
+        public void DeleteEmployee_WhenCalled_DeleteTheEmployeeFromDb()
+        {
+            var storage = new Mock<IEmployeeStorage>();
+            var controller = new EmployeeController(storage.Object);
+
+            controller.DeleteEmployee(1);
+
+            storage.Verify(s => s.DeleteEmployee(1));
+        }
+
+        [Test]
+        public void DeleteEmployee_WhenCalled_ReturnRedirectObject()
+        {
+            var storage = new Mock<IEmployeeStorage>();
+            var controller = new EmployeeController(storage.Object);
+
+            var result = controller.DeleteEmployee(1);
+
+            Assert.That(result.GetType() == typeof(RedirectResult));
+        }
+    }
+}
